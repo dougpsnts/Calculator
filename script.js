260138c8d0071031831a6c950ffd7b29e220c071
Douglas Santos <engdouglasps@gmail.com>
2025-07-11 22:22:18 -0300

    Adding the function calculate to the equal key and the enter

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 const input = document.getElementById('input');
 const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
 
-//new code starts here
+// Capture the value of the keys when pressed
 
 input.addEventListener('keydown', function (ev) {
+	// Prevent default behavior for keys that are not allowed
 	ev.preventDefault();
 	if (allowedKeys.includes(ev.key)) {
 		input.value += ev.key;
 		return
 	}
+	// Handle special keys
 	if (ev.key === 'Backspace') {
-		input.value = input.value.slice(0, -1);
+	input.value = input.value.slice(0, -1);
 	}	
+	if (ev.key === "Enter") {
+    calculate()
+  }
 })
 
+document.getElementById("equal").addEventListener("click", calculate)
+
+// Capture the value of the buttons when clicked
 
 document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
 	charKeyBtn.addEventListener("click", function () {
@@ -21,3 +29,10 @@ document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
 		input.value += value;
 	});
 })
+
+// Add the calc logic
+
+function calculate() {
+	const result = eval(input.value)
+	input.value = result
+}
